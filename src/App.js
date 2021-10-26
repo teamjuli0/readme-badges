c1c8df4c98f929425b4f39fff0875a048d64d18c
Julio Valdez <valdezjulio95@gmail.com>
2021-10-26 01:15:31 -0500

    added alpha build banner

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ import { Home } from './views'
 function App() {
   return (
     <>
+      <div
+        style={{
+          width: '100%',
+          height: '48px',
+          padding: '0 0 0 10px',
+          backgroundColor: '#da4040',
+          fontFamily: 'NotoSans',
+          color: 'white',
+          fontWeight: 'bold',
+          fontSize: '30px',
+        }}
+      >
+        ALPHA BUILD
+      </div>
       <Navbar />
       <Home />
     </>
