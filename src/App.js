4353b7b065e453c86e6599f3aacda6e364639d59
Julio Valdez <valdezjulio95@gmail.com>
2021-11-23 14:10:00 +0500

    fixed window location on copy

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import { Routes, Route } from 'react-router-dom'
 function App() {
   const getClipboardCopy = () => {
     const urlArr = window.location.href.split('/')
-    console.log(urlArr[urlArr.length - 2])
+    const theme = urlArr[urlArr.length - 3]
+    const type = urlArr[urlArr.length - 2]
+    const name = urlArr[urlArr.length - 1]
+
     navigator.clipboard
       .writeText(
-        `## <a href="#readme-badges"><img id="usage" src="https://github.com/teamjuli0/readme-badges/blob/main/themes/${
-          urlArr[urlArr.length - 3]}/${
-          urlArr[urlArr.length - 2]
-        }/${urlArr[urlArr.length - 1]}.png?raw=true" style="height: 25px"></a>`
+        `## <a href="#readme-badges"><img id="usage" src="https://github.com/teamjuli0/readme-badges/blob/main/themes/${theme}/${type}/${name}.png?raw=true" style="height: 25px"></a>`
       )
-      .then(
-        () =>
-          (window.location = `https://github.com/teamjuli0/readme-badges/#${
-            urlArr[urlArr.length - 1]
-          }-badge`)
-      )
+      .then(() => {
+        const wikiName = theme
+          .split('-')
+          .map((name) => name[0].toUpperCase() + name.substring(1))
+          .join('-') + '-Theme'
+
+          window.location = `https://github.com/teamjuli0/readme-badges/wiki/${wikiName}`
+      })
   }
 
   return (
